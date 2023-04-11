7194f8db91d0ef67dff8348eebc1497c599468e5
Zaradin <crottyjj@hotmail.com>
2023-04-11 17:45:32 +0100

    JUnit tests for listing receipts from the collection

--- src/test/kotlin/controllers/ReceiptAPITest.kt
+++ src/test/kotlin/controllers/ReceiptAPITest.kt
@@ -4,6 +4,7 @@ import models.Receipt
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
 import kotlin.test.assertEquals
@@ -37,21 +38,46 @@ class ReceiptAPITest {
         emptyReceipts = null
     }
 
-    @Test
-    fun `adding a receipt to a populated list adds to ArrayList`(){
-        val newReceipt = Receipt("Dunnes", "Grocery", "Grocery Shopping", LocalDate.now(), "Credit")
-        assertEquals(3, populatedReceipts!!.numberOfReceipts())
-        assertTrue(populatedReceipts!!.add(newReceipt))
-        assertEquals(4, populatedReceipts!!.numberOfReceipts())
-        assertEquals(newReceipt, populatedReceipts!!.findReceipt(populatedReceipts!!.numberOfReceipts() - 1))
+    @Nested
+    inner class AddReceipts{
+        @Test
+        fun `adding a receipt to a populated list adds to ArrayList`(){
+            val newReceipt = Receipt("Dunnes", "Grocery", "Grocery Shopping", LocalDate.now(), "Credit")
+            assertEquals(3, populatedReceipts!!.numberOfReceipts())
+            assertTrue(populatedReceipts!!.add(newReceipt))
+            assertEquals(4, populatedReceipts!!.numberOfReceipts())
+            assertEquals(newReceipt, populatedReceipts!!.findReceipt(populatedReceipts!!.numberOfReceipts() - 1))
+        }
+
+        @Test
+        fun `adding a receipt to an empty list adds to ArrayList`(){
+            val newReceipt = Receipt("Amazon", "Electronics", "New Laptop", LocalDate.now(), "Debit")
+            assertEquals(0, emptyReceipts!!.numberOfReceipts())
+            assertTrue(emptyReceipts!!.add(newReceipt))
+            assertEquals(1, emptyReceipts!!.numberOfReceipts())
+            assertEquals(newReceipt, emptyReceipts!!.findReceipt(emptyReceipts!!.numberOfReceipts() - 1))
+        }
     }
 
-    @Test
-    fun `adding a receipt to an empty list adds to ArrayList`(){
-        val newReceipt = Receipt("Amazon", "Electronics", "New Laptop", LocalDate.now(), "Debit")
-        assertEquals(0, emptyReceipts!!.numberOfReceipts())
-        assertTrue(emptyReceipts!!.add(newReceipt))
-        assertEquals(1, emptyReceipts!!.numberOfReceipts())
-        assertEquals(newReceipt, emptyReceipts!!.findReceipt(emptyReceipts!!.numberOfReceipts() - 1))
+    @Nested
+    inner class ListReceipts{
+        @Test
+        fun `listAllReceipts returns No Receipts Stored message when ArrayList is empty`() {
+            assertEquals(0, emptyReceipts!!.numberOfReceipts())
+            assertTrue(emptyReceipts!!.listAllReceipts().lowercase().contains("no receipts"))
+        }
+
+        @Test
+        fun `listAllReceipts returns Receipts when ArrayList has receipts stored`() {
+            assertEquals(3, populatedReceipts!!.numberOfReceipts())
+            val receiptsString = populatedReceipts!!.listAllReceipts().lowercase()
+            assertTrue(receiptsString.contains("dunnes"))
+            assertTrue(receiptsString.contains("tk maxx"))
+            assertTrue(receiptsString.contains("currys"))
+            assertTrue(receiptsString.contains("grocery"))
+            assertTrue(receiptsString.contains("clothing"))
+            assertTrue(receiptsString.contains("electronics"))
+        }
+
     }
 }
